5347de36aaf0301602adb18b8fcc24dd19d772b2
FANGPLAY SIRIRAK <fangplay_2014@hotmail.com>
2023-03-23 11:11:49 +0700

    add insert processing

--- server.js
+++ server.js
@@ -13,54 +13,46 @@ const adapter = new FileSync(path.join(__dirname, './db.json'));
 const db = low(adapter);
 
 app.get("/", (req, res) => {
-  // res.send("Index Page System");
-  // return res.status(200);
   res.sendFile(path.join(__dirname, "public", "./views/index.html"));
 });
 
 app.get('/diary-list', (req, res) => {
-  // const diaries = db.get('diary').value();
-  // return res.send(diaries).status(200);
   res.sendFile(path.join(__dirname, "public", "./views/diary-list.html"));
 })
 
 app.get('/os-list', (req, res) => {
-  // const os = db.get('os').value();
-  // return res.json(os).status(200);
   res.sendFile(path.join(__dirname, "public", "./views/os-list.html"));
 })
 
-app.get('/diary-add', (req, res) =>{
-  // const data = {
-  //   id:nanoid.nanoid(),
-  //   title:req.body.title,
-  //   description:req.body.description,
-  //   date:req.body.date
-  // }
-  // db.get('diary')
-  // .push(data)
-  // .last()
-  // .write()
-  // return res.status(200).send(
-  //   db.get('diary').last().value()
-  // );
+app.get('/diary-add', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "./views/add-diaries.html"));
 })
 
-app.get('/os-add', (req, res) =>{
-  // const data = {
-  //   id:nanoid.nanoid(),
-  //   title:req.body.title,
-  //   type:req.body.type
-  // }
-  // db.get('os')
-  // .push(data)
-  // .last()
-  // .write()
-  // return res.status(200).send(
-  //   db.get('os').last().value()
-  // );
+app.get('/os-add', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "./views/add-os.html"));
+})
+
+app.post('/diary-save',(req,res) => {
+  db.read();
+  var diary = {
+    id : nanoid.nanoid(),
+    title : req.body.title,
+    description : req.body.description,
+    date : req.body.date
+  }
+  db.get('diary').push(diary).last().write();
+  res.redirect('/diary-add');
+})
+
+app.post('/os-save',(req,res) => {
+  db.read();
+  var os = {
+    id : nanoid.nanoid(),
+    osname : req.body.name,
+    type : req.body.type
+  }
+  db.get('os').push(os).last().write();
+  res.redirect('/os-add');
 })
 
 app.listen(port, () => {
