28dc4b4a060fb7686bb653056d75947333d4d25e
FANGPLAY SIRIRAK <fangplay_2014@hotmail.com>
2023-08-11 15:20:07 +0700

    update data processing

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const port = process.env.PORT || 3000;
 const adapter = new FileSync(path.join(__dirname, './db.json'));
 const db = low(adapter);
 
+// const methodOverride = require("method-override");
+// router.use(methodOverride("_method", {
+//   methods: ["POST", "GET"]
+// }));
+
 // set view engine
 app.set('view engine', 'html');
 app.set('view engine', 'ejs');
@@ -47,6 +52,57 @@ app.get('/os-add', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "./views/add-os.html"));
 })
 
+app.get('/diary-edit?${diary.id}', (req,res,next) => {
+  let diaryID = req.param.id;
+  Diary.findById(diaryID)
+    .then(diary => {
+      res.render('diary-edit',{
+        diary: diary
+      });
+    })
+    .catch(error => {
+      alert('Error updating diary by ID: ${error.message}');
+      next(error);
+    });
+})
+
+app.put('/diary-update?${diary.id}', (req,res,next) => {
+  let diaryID = req.param.id,
+  diaryParams = {
+    title : req.body.title,
+    description : req.body.description,
+    date : req.body.date
+  };
+
+  Diary.findByIdAndUpdate(diaryID, {
+    $set: diaryParams
+  }).then(
+    diary => {
+      alert('Updated diary data complete');
+      rea.locals.redirect = '/diary-list';
+      res.locals.diary = diary;
+      next();
+    }
+  ).catch(error => {
+    alert('Error updating diary by ID: ${error.message}');
+    next(error);
+  });
+})
+
+app.delete('/diary-delete?${diary.id}', (req,res,next) => {
+  let diaryID = req.param.id;
+  Diary.findByIdAndRemove(diaryID)
+  .then(() => {
+    alert('Deleted diary data complete');
+    res.locals.redirect = "/diary-list";
+    next();
+  }).catch(error => {
+      alert('Error deleting diary by ID: ${error.message}');
+      next(error);
+  });
+})
+
+
 app.post('/diary-save',(req,res) => {
   db.read();
   var diary = {
@@ -56,6 +112,7 @@ app.post('/diary-save',(req,res) => {
     date : req.body.date
   }
   db.get('diary').push(diary).last().write();
+  alert('Added diary data complete');
   res.redirect('/diary-add');
 })
 
@@ -70,6 +127,7 @@ app.post('/os-save',(req,res) => {
   res.redirect('/os-add');
 })
 
+
 app.listen(port, () => {
   console.log("Server started on http://localhost:" + port);
-});
+})
